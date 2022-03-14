2159b7066d19a00c4a3f7bffb2e300cc0a98c6c8
alihduong <nguyenhoangduong0711@gmail.com>
2022-03-14 10:42:33 +0700

    commit

---
+++ src/main/java/cg/casestudy_socialnetwork/CaseStudySocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package cg.casestudy_socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudySocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudySocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cg/casestudy_socialnetwork/CaseStudySocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.casestudy_socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudySocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
