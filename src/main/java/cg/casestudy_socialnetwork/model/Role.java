f098ae13da88e7443ad26d1dd0cdee803961b682
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-03-14 11:21:20 +0700

    commit first

---
+++ src/main/java/cg/casestudy_socialnetwork/model/Role.java
@@ -0,0 +1,18 @@
+package cg.casestudy_socialnetwork.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+}
